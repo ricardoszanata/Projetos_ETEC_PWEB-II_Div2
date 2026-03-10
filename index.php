a671afd3fd75598aa83694c0a0546d114d282295
otavioandrade-afk <otaviopandrade2010@gmail.com>
2026-03-10 12:36:50 -0300

    acerto do card e utilização da condição while

--- index.php
+++ index.php
@@ -139,8 +139,10 @@ $quantidade = 5;
           <img src="<?php echo $imagem; ?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?php echo $titulo; ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text"><?php echo $resumo; ?></p>
+            <p class="text-danger">Valor:<b> R$ <?php echo $valor; ?></b></p>
+            <p class="text-primary">Estoque: <?php echo $quantidade; ?></p>
+            <a href="#" class="btn btn-primary">Ver Produto</a>
           </div>
         </div>
       </div>
